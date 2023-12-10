b4f130881f1ea6ae373dff26cfa7fa0c83a8df8a
hsuankai.chang <hsuankai.chang@driveseres.com>
2023-12-10 13:11:50 -0800

    Qt widgets for begineers with c++ section 5 finished

---
+++ QtWidgetsForBegineersWtihC++/Resources/qtwb-5-4/MyApp/dialog.cpp
@@ -0,0 +1,25 @@
+#include "dialog.h"
+#include "ui_dialog.h"
+
+Dialog::Dialog(QWidget *parent)
+    : QDialog(parent)
+    , ui(new Ui::Dialog)
+{
+    ui->setupUi(this);
+    MyLib mylib;
+    ui->lineEdit->setText(mylib.message());
+
+    QPixmap img(":/files/images/anchor.png");
+    ui->label->setPixmap(img);
+}
+
+Dialog::~Dialog()
+{
+    delete ui;
+}
+
+void Dialog::on_buttonBox_accepted()
+{
+    accept();
+}
+
